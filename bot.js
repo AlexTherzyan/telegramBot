5b83a5d27dee1c485dcef2f1ec607db6dbe1bd4d
a_terzjan <buyka@tut.by>
2018-03-21 16:09:14 +0300

    Добавлен курс валют

--- bot.js
+++ bot.js
@@ -45,16 +45,27 @@ case keyboardClick.back:
 // обрабатываем нажатия кнопок inline клавиатуры
 bot.on('callback_query', query =>{
     const base = query.data
-    const symbol = 'RUB'
+    const symbol = 'RUB' //BYN
+
+    // всплывающее сообщение
+    bot.answerCallbackQuery({
+        callback_query_id: query.id,
+        text: `Вы выбрали ${base}`
+    })
     //http://data.fixer.io/api/latest?symbols=${symbol}&base=${base}&access_key=91f6c9250867b8b4badb15c009f66c86
-    request(`http://data.fixer.io/api/latest?access_key=91f6c9250867b8b4badb15c009f66c86&base=${base}&symbols=${symbol}`, (error,response,body) =>{
+    request(`http://api.fixer.io/latest?symbols=${symbol}&base=${base}`, (error,response,body) =>{
+
         if(error) throw new Error(error)
-
-
+        // если успешный запрос
         if(response.statusCode === 200){
             // парсим
             const currencyData = JSON.parse(body)
-            console.log(currencyData)
+            //console.log(currencyData)
+
+            const html = `<b>$1 ${base} </b> - <em>${currencyData.rates[symbol]} ${symbol}</em>`
+            bot.sendMessage(query.message.chat.id,html, {
+                parse_mode: 'HTML' // говорим боту что передаем данные в html
+            })
         }
    })
     //console.log(JSON.stringify(query,null,2))
